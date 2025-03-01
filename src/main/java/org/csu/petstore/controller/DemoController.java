1c50f63f4c8bc94de76eadb564acf30530a7bac8
Microindole <1513979779@qq.com>
2025-03-01 16:09:27 +0800

    init project

---
+++ src/main/java/org/csu/petstore/controller/DemoController.java
@@ -0,0 +1,26 @@
+package org.csu.petstore.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+
+
+
+    @GetMapping("/demo")
+    @ResponseBody
+    public String demo(){
+        return "demo";
+    }
+
+    @GetMapping("/demo1")
+    public String demo1(Model model){
+        model.addAttribute("message", "hello thymeleaf");
+        return "demo1";
+    }
+}
---
+++ src/main/java/org/csu/petstore/entity/Category.java
@@ -0,0 +1,17 @@
+package org.csu.petstore.entity;
+
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("category")
+public class Category {
+    @TableId(value = "catId")
+    private String categoryId;
+    private String name;
+    @TableField("descn")
+    private String description;
+}
